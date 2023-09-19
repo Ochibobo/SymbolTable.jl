f865d3d9112e77cf0446c36dac4502c3f2ad1445
Warren Ochibobo <ochibobowarren@gmail.com>
2023-09-19 20:49:09 +0300

    Initial commit of the BTree data structure

---
+++ src/BTree/BTree.jl
@@ -0,0 +1,11 @@
+module BTree
+## Implementation of a BTree Data Structure
+
+"""
+    const M = Ref(4)
+
+The `number of entries` in a `node`. This value can be altered to a value that you find convenient.
+"""
+const M = Ref(4)
+
+end
